390cb83ade54be66842a169b214cc5186b7d0a9a
Louis Bima Susanto <69kamikazesusanto69@gmail.com>
2026-03-06 14:52:36 +0700

    fix prisma client

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient()
+import { prisma } from "@/lib/prisma"
 
 // GET all events
 export async function GET(request: Request) {
